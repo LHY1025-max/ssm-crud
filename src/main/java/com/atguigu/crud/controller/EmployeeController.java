f96da7e676a1f2db651dfeccf2fbb8b21d0b875c
luohaoyi <534131274@qq.com>
2021-08-14 22:56:28 +0800

    版本二：使用ajax和json实现完整的CRUD

--- src/main/java/com/atguigu/crud/controller/EmployeeController.java
+++ src/main/java/com/atguigu/crud/controller/EmployeeController.java
@@ -7,10 +7,13 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -57,4 +60,101 @@ public class EmployeeController {
         PageInfo page = new PageInfo(emps,5);
         return Msg.success().add("pageInfo", page);
     }
+
+    /**
+     * 保存员工数据
+     * 1、JSR303校验
+     * 2、导入Hibernate-Validator
+     * @param employee
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value="/emp", method= RequestMethod.POST)
+    public Msg saveEmp(@Valid Employee employee, BindingResult result){
+        if (result.hasErrors()){
+            //校验失败，返回失败，在模态框中显示校验失败的错误信息
+            Map<String,Object> map = new HashMap<>();
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                map.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return Msg.fail().add("errorFields", map);
+        }else {
+            employeeService.saveEmp(employee);
+            return Msg.success();
+        }
+    }
+
+    /**
+     * 校验员工姓名数据
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping("/checkUser")
+    public Msg checkUserName(@RequestParam("empName")String empName) {
+        String regx = "(^[a-zA-Z0-9_-]{6,16}$)|(^[\u2E80-\u9FFF]{2,5})";
+        if (!empName.matches(regx)){
+            return Msg.fail();
+        }
+        boolean b = employeeService.checkUserName(empName);
+        if (b) {
+            return Msg.success();
+        } else {
+            return Msg.fail();
+        }
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/emp/{id}", method = RequestMethod.GET)
+    public Msg getEmp(@PathVariable("id")Integer id){
+        Employee emp = employeeService.getEmp(id);
+        return Msg.success().add("emp", emp);
+    }
+
+    /**
+     * 根据id修改员工信息
+     * @param
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/emp/{empId}", method = RequestMethod.PUT)
+    public Msg updateEmp(Employee employee){
+        employeeService.updateEmp(employee);
+        return Msg.success();
+    }
+
+    /**
+     * 根据id删除员工
+     * @param id
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/emp/{id}", method = RequestMethod.DELETE)
+    public Msg deleteEmp(@PathVariable("id")Integer id){
+        employeeService.deleteEmp(id);
+        return Msg.success();
+    }
+
+    /**
+     * 根据id删除多个员工
+     * @param ids
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/deleteEmps/{ids}", method = RequestMethod.DELETE)
+    public Msg deleteEmps(@PathVariable("ids")String ids){
+        String[] splitId = ids.split(",");
+        List<Integer> idList = new ArrayList<>();
+        for (String s : splitId) {
+            int empId = Integer.parseInt(s);
+            idList.add(empId);
+        }
+        employeeService.deleteBatch(idList);
+        return Msg.success();
+    }
+
 }
--- src/main/java/com/atguigu/crud/service/EmployeeService.java
+++ src/main/java/com/atguigu/crud/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.atguigu.crud.service;
 
 import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.bean.EmployeeExample;
 import com.atguigu.crud.dao.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,4 +21,62 @@ public class EmployeeService {
         return employeeMapper.selectByExampleWithDept(null);
     }
 
+    /**
+     * 保存员工
+     * @param employee
+     * @return
+     */
+    public int saveEmp(Employee employee){
+        return employeeMapper.insertSelective(employee);
+    }
+
+    /**
+     * 校验用户名是否可用
+     * @param empName
+     * @return
+     */
+    public boolean checkUserName(String empName) {
+        EmployeeExample example = new EmployeeExample();
+        EmployeeExample.Criteria criteria = example.createCriteria();
+        criteria.andEmpNameEqualTo(empName);
+        Long count= employeeMapper.countByExample(example);
+        return count==0;
+    }
+
+    /**
+     * 查询员工信息
+     * @return
+     */
+    public Employee getEmp(Integer id) {
+        return employeeMapper.selectByPrimaryKeyWithDept(id);
+    }
+
+    /**
+     * 修改员工信息
+     * @param employee
+     * @return
+     */
+    public int updateEmp(Employee employee) {
+        return employeeMapper.updateByPrimaryKeySelective(employee);
+    }
+
+    /**
+     * 删除员工信息
+     * @param id
+     * @return
+     */
+    public int deleteEmp(Integer id) {
+        return employeeMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 删除多位员工信息
+     * @param idList
+     */
+    public int deleteBatch(List<Integer> idList) {
+        EmployeeExample employeeExample = new EmployeeExample();
+        EmployeeExample.Criteria criteria = employeeExample.createCriteria();
+        criteria.andEmpIdIn(idList);
+        return employeeMapper.deleteByExample(employeeExample);
+    }
 }
